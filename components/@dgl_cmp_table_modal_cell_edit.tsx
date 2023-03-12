c710be942b8aa465a4eafbc0d1281b86215c7d02
dinogomez <dinogomez117@gmail.com>
2023-03-12 21:11:38 +0800

    Added Update Functionality

--- components/@dgl_cmp_table_modal_cell_edit.tsx
+++ components/@dgl_cmp_table_modal_cell_edit.tsx
@@ -1,19 +1,60 @@
-import { Row, Modal, Input, Checkbox, Text, Button } from "@nextui-org/react";
+import {
+  Row,
+  Modal,
+  Input,
+  Checkbox,
+  Text,
+  Button,
+  Grid,
+} from "@nextui-org/react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { DeleteIcon, IconButton, EditIcon } from "./svg/@dgl_svg";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
 import InputGenerator from "./@dgl_cmp_field_input_generator";
 
-export default function ModalCellEdit({ closeHandler, visible, menu }: any) {
+export default function ModalCellEdit({
+  closeHandler,
+  visible,
+  selectedRow,
+  currentTable,
+}: any) {
+  const user = useUser();
+
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [rowData, setRowData] = useState<any[]>([]);
   const [keys, setKeys] = useState<any[]>([]);
   const [values, setValues] = useState<any[]>([]);
 
+  const updateInitialStateFromRow = () => {
+    const initialState: any = {};
+    keys.map((key: any, index) => {
+      initialState[key] = values[index];
+    });
+    setRowData(initialState);
+  };
+
   useEffect(() => {
-    setKeys(Object.keys(menu));
-    setValues(Object.values(menu));
-  }, [menu]);
+    setKeys(Object.keys(selectedRow));
+    setValues(Object.values(selectedRow));
+    //console.log(typeof keys[0] + ":" + keys[0]);
+  }, [selectedRow]);
+
+  const handleChange = (e: any) => {
+    setRowData({ ...rowData, [e.target.name]: e.target.value });
+  };
+
+  const updateRow = async () => {
+    try {
+      const { data, error } = await supabaseClient
+        .from(currentTable)
+        .update([rowData])
+        .eq("id", values[0]);
+      if (error) throw error;
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
 
   const exclude = ["inserted_at", "updated_at", "created_at", "id"];
 
@@ -26,6 +67,9 @@ export default function ModalCellEdit({ closeHandler, visible, menu }: any) {
     >
       <Modal.Header>
         <Text h3>Update Row</Text>
+        <Grid xs={12}>
+          <Text>Editing as {user?.email}</Text>
+        </Grid>
       </Modal.Header>
       <Modal.Body>
         <>
@@ -36,6 +80,7 @@ export default function ModalCellEdit({ closeHandler, visible, menu }: any) {
                   labelLeft="Read Only"
                   readOnly
                   underlined
+                  name={key}
                   label={key.toUpperCase()}
                   placeholder="Guillermo Rauch"
                   initialValue={values[index]}
@@ -46,8 +91,10 @@ export default function ModalCellEdit({ closeHandler, visible, menu }: any) {
                 <Input
                   shadow
                   clearable
+                  name={key}
                   label={key.toUpperCase()}
                   placeholder="Guillermo Rauch"
+                  onChange={handleChange}
                   initialValue={values[index]}
                 />
               );
@@ -56,7 +103,7 @@ export default function ModalCellEdit({ closeHandler, visible, menu }: any) {
         </>
       </Modal.Body>
       <Modal.Footer>
-        <Button auto onPress={closeHandler} color="success">
+        <Button auto onPress={updateRow} color="success">
           Update
         </Button>
       </Modal.Footer>
--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,16 +1,8 @@
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react"; 
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import {
-  Table,
-  Text,
-  Dropdown,
-  Loading,
-  Grid,
-  Spacer,
-  Button,
-} from "@nextui-org/react";
+import { Table, Text, Dropdown, Loading, Grid, Spacer, Button, Col, Row, } from "@nextui-org/react"
 import { useRouter } from "next/router";
-import { GetServerSidePropsContext } from "next";
+import { GetServerSidePropsContext } from 'next';
 import { useState, useEffect, useMemo, useReducer } from "react";
 
 import Link from "next/link";
@@ -19,114 +11,128 @@ import React from "react";
 import TableDynamic from "../../components/@dgl_cmp_table_dynamic";
 import ModalCellEdit from "../../components/@dgl_cmp_table_modal_cell_edit";
 
+
 export default function Menu() {
-  //Supabase Database Handler
-  const supabaseClient = useSupabaseClient();
-  //User State from Supabase
-  const user = useUser();
+    //Supabase Database Handler
+    const supabaseClient = useSupabaseClient();
+    //User State from Supabase
+    const user = useUser();
 
-  const [dbtables, setDbTables] = useState<any[]>([]);
-  const [tableCols, setTableCols] = useState<any[]>([]);
-  const [selectedRow, setSelectedRow] = useState<any>([]);
-  const [tableRows, setTableRows] = useState<any[]>([]);
-  const [selected, setSelected] = useState<any>(new Set([""]));
-  const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
-  const [visible, setVisible] = useState(false);
-  const [selectedId, setSelectedId] = useState(0);
+    
+    const [dbtables, setDbTables] = useState<any[]>([]);
+    const [tableCols, setTableCols] = useState<any[]>([]);
+    const [selectedRow, setSelectedRow] = useState<any>([]);
+    const [tableRows, setTableRows] = useState<any[]>([]);        
+    const [selected, setSelected] = useState<any>(new Set(['']));
+    const [ignored, forceUpdate] = useReducer(x=> x+1, 0);
+    const [visible, setVisible] = useState(false);
+    const [selectedId, setSelectedId] = useState(0);
 
-  const handler = (row: any) => {
-    setVisible(true);
-    setSelectedRow(row);
-  };
-  const closeHandler = () => {
-    setVisible(false);
-    console.log("closed");
-  };
+    const handler = (row:any) => {
+      setVisible(true);
+      setSelectedRow(row);
+    }
+    const closeHandler = () => {
+      setVisible(false);
+      console.log("closed");
+    };
 
-  const removeFromRows = (id: any) => {
-    setTableRows((prevState) =>
+    const removeFromRows = (id:any) => {
+      setTableRows((prevState) =>
       prevState.filter((prevItem) => prevItem !== id)
     );
-    deleteRowFromTable(id);
-  };
+      deleteRowFromTable(id)
+    };
 
-  const deleteRowFromTable = async (id: any) => {
-    try {
-      const { data, error } = await supabaseClient
-        .from(selectedValue)
-        .delete()
-        .eq("id", id);
-      if (error) throw error;
-    } catch (error: any) {
-      alert(error.message);
+    const deleteRowFromTable = async (id:any) => {
+      try{
+        const { data, error} = await supabaseClient
+          .from(selectedValue)
+          .delete()
+          .eq("id", id);
+        if(error) throw error;
+      }catch(error:any){
+        alert(error.message);
+      }
     }
-  };
+    
+    let selectedValue = useMemo(
+      () => Array.from(selected).join("").replaceAll("_", " "),[selected]
+    );
 
-  let selectedValue = useMemo(
-    () => Array.from(selected).join("").replaceAll("_", " "),
-    [selected]
-  );
 
-  const getData = async () => {
-    if (selectedValue) {
-      let { data, error } = await supabaseClient
-        .from(selectedValue)
-        .select("*");
-      if (data != null && data != undefined) {
-        let columns: any = [];
+    const getData = async() => {
+      if(selectedValue){
+      let { data, error } = await supabaseClient.from(selectedValue).select("*");
+      if(data != null && data != undefined){
 
-        Object.keys(data[0]).forEach((element) => {
+
+        let columns:any = [];
+
+        Object.keys(data[0]).forEach(element => {
           let data = {
             key: element,
-            label: element.toUpperCase(),
-          };
+            label: element.toUpperCase()
+          }
           columns.push(data);
         });
 
         const action = {
           key: "action",
-          label: "ACTION",
-        };
-
+          label: "ACTION"
+        }
+    
         columns.push(action);
 
         setTableCols(columns);
         setTableRows(data);
+
       } else {
         getData();
       }
-    }
-  };
-  const getTables = async () => {
-    try {
-      let { data, error } = await supabaseClient.rpc("gettables");
-
-      if (data != null) {
-        setDbTables(data);
-        if (selectedValue === undefined) {
-          selectedValue = data[0].table_name;
-          setSelected(data[0].table_name);
-        }
+    
+  }
+}
+const getTables = async() => {
+  try{
+    let { data, error } = await supabaseClient.rpc('gettables')
+ 
+    if(data != null){
+      setDbTables(data);
+      if(selectedValue === undefined){
+        selectedValue = data[0].table_name;
+        setSelected(data[0].table_name)
       }
-    } catch (error: any) {
-      <Loading>Loading</Loading>;
     }
-  };
 
-  useEffect(() => {
-    //const {tableCols, tableRows} = props;
-    console.log(selectedRow);
-    if (typeof selectedValue !== "undefined") {
-      getTables();
+  }catch(error: any){
+    <Loading>Loading</Loading>
+  }
+}
+   
+    
+    useEffect(()=>{   
+      
+      //const {tableCols, tableRows} = props;
+      console.log(selectedRow);
+      if(typeof selectedValue !== "undefined") {
+        getTables();
+        getData();
+
+    }
+
+      
+    },[selected]) 
+
+    useEffect(()=>{
       getData();
-    }
-  }, [selected]);
+    },[tableRows])
 
-  useEffect(() => {
-    getData();
-  }, [tableRows]);
+  
 
-  /*
+
+
+    /*
         console.log(dbtables[0].table_name)
     console.log(tableRows)
     useEffect(()=>{
@@ -134,77 +140,91 @@ export default function Menu() {
     },[selectedValue]) 
     */
 
-  return (
-    <>
-      <Text h2>Tables</Text>
-      <Text size={"$lg"} css={{ my: "$8" }}>
-        Dynamic Tables fetched from cloud Postgres Database
-      </Text>
-      <hr />
-      <br />
-      <Dropdown>
-        <Dropdown.Button shadow color="secondary">
-          {!selectedValue ? "Select Table" : selectedValue}
-        </Dropdown.Button>
-        <Dropdown.Menu
-          aria-label="Dynamic Actions"
-          items={dbtables}
-          color="secondary"
-          disallowEmptySelection
-          selectionMode="single"
-          selectedKeys={selected}
-          onSelectionChange={setSelected}
-        >
-          {(dbtable: any) => (
-            <Dropdown.Item key={dbtable.table_name}>
-              {dbtable.table_name}
-            </Dropdown.Item>
-          )}
-        </Dropdown.Menu>
-      </Dropdown>
 
-      <ModalCellEdit
-        visible={visible}
-        closeHandler={closeHandler}
-        menu={selectedRow}
-      />
 
-      {/*<TableDynamic columns={tableCols} rows={tableRows} />*/}
-      {tableCols.length === 0 ? (
-        <Text></Text>
-      ) : (
-        <TableDynamic
-          table={selectedValue}
-          tableCols={tableCols}
-          tableRows={tableRows}
-          removeRow={removeFromRows}
-          editRow={handler}
-        />
-      )}
+
+
+    return (
+        
+      <>
+        
+        <Text h2>Tables</Text>
+        <Text size={"$lg"} css={{my: "$8"}}>Dynamic Tables fetched from cloud Postgres Database</Text>
+        <hr />
+        <br />
+        <Row justify="center" align="center">
+              <Col css={{ d: "flex" }}>
+              <Dropdown>
+                <Dropdown.Button  shadow color="secondary">        
+                {
+                  !selectedValue ? "Select Table" : selectedValue
+                }
+                </Dropdown.Button>
+                <Dropdown.Menu 
+                aria-label="Dynamic Actions" 
+                items={dbtables} color="secondary"
+                disallowEmptySelection
+                selectionMode="single"
+                selectedKeys={selected}
+                onSelectionChange={setSelected}>
+                  {(dbtable: any) => (
+                    <Dropdown.Item
+                      key={dbtable.table_name}
+                    >
+                      {dbtable.table_name}
+                    </Dropdown.Item>
+                    
+                  )}
+                </Dropdown.Menu>
+    </Dropdown>
+              </Col>
+              <Col css={{ d: "flex" }}></Col>
+
+        </Row>
+        
+
+
+    <ModalCellEdit visible={visible} closeHandler={closeHandler} selectedRow={selectedRow} currentTable={selectedValue} />
+   
+            
+    {/*<TableDynamic columns={tableCols} rows={tableRows} />*/}
+    {
+      tableCols.length === 0 
+      ? 
+      <Text></Text>
+      : 
+      <TableDynamic 
+        table={selectedValue} 
+        tableCols={tableCols} 
+        tableRows={tableRows} 
+        removeRow={removeFromRows} 
+        editRow={handler}/>
+    }
+      
     </>
-  );
-}
+    )
+  }
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  // Create authenticated Supabase Client
-  const supabase = createServerSupabaseClient(ctx);
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+// Create authenticated Supabase Client
+const supabase = createServerSupabaseClient(ctx);
+// Check if we have a session
+const {
+    data: { session }
+    } = await supabase.auth.getSession();
 
-  if (!session)
+if (!session)
     return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+    redirect: {
+        destination: '/',
+        permanent: false
+    }
     };
 
-  return {
+return {
     props: {
-      initialSession: session,
-      user: session.user,
+    initialSession: session,
+    user: session.user,
     },
-  };
+};
 };
